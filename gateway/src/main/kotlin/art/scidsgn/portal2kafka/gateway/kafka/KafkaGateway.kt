f0c940a25cab9f9b7f504121d6ef7fd8c459b246
Kacper Seredyn <kacper17@outlook.com>
2023-04-23 00:19:26 +0200

    gateway: send events to Kafka topic

---
+++ gateway/src/main/kotlin/art/scidsgn/portal2kafka/gateway/kafka/KafkaGateway.kt
@@ -0,0 +1,21 @@
+package art.scidsgn.portal2kafka.gateway.kafka
+
+import art.scidsgn.portal2kafka.gateway.dto.PortalEventDto
+import org.apache.kafka.clients.admin.NewTopic
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.KafkaHeaders
+import org.springframework.messaging.support.MessageBuilder
+import org.springframework.stereotype.Service
+
+@Service
+class KafkaGateway(val gatewayTopic: NewTopic, val kafkaTemplate: KafkaTemplate<String, PortalEventDto>) {
+
+    fun send(eventDto: PortalEventDto) {
+        val message = MessageBuilder
+            .withPayload(eventDto)
+            .setHeader(KafkaHeaders.TOPIC, gatewayTopic.name())
+            .build()
+
+        kafkaTemplate.send(message)
+    }
+}
--- gateway/src/main/kotlin/art/scidsgn/portal2kafka/gateway/rest/PortalController.kt
+++ gateway/src/main/kotlin/art/scidsgn/portal2kafka/gateway/rest/PortalController.kt
@@ -1,6 +1,7 @@
 package art.scidsgn.portal2kafka.gateway.rest
 
 import art.scidsgn.portal2kafka.gateway.dto.PortalEventDto
+import art.scidsgn.portal2kafka.gateway.kafka.KafkaGateway
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
 @Controller
-class PortalController {
+class PortalController(val kafkaGateway: KafkaGateway) {
     @PostMapping("/portal")
     fun receiveEvent(@RequestBody event: PortalEventDto): ResponseEntity<String> {
         println(event)
+        kafkaGateway.send(event)
+
         return ResponseEntity.status(HttpStatus.OK).body("")
     }
 }
